f522fe19e5fe54b7b4d26eff862ed13c256869dc
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2024-12-20 18:02:04 +0530

    added repository layer

---
+++ src/controllers/product_controller.js
@@ -0,0 +1,65 @@
+const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+
+const ProductService = require('../services/product_service');
+const FakeStoreRepository = require('../repositories/fake_store_repository');
+const productService = new ProductService(new FakeStoreRepository());
+
+function createProduct(req, res) {
+
+    try {
+
+        const response = productService.createProduct(req.body);
+
+        return res
+                .status(StatusCodes.CREATED)
+                .json({
+                    sucess: true,
+                    error: {},
+                    message: ReasonPhrases.CREATED + " Product",
+                    data: response
+        });
+
+    } catch(error) {
+        console.log("Something went wrong", error);
+    }
+
+}
+
+async function getProducts(req, res) {
+    try {
+        const response = await productService.getProducts();
+        return res
+                .status(StatusCodes.OK)
+                .json({
+                    sucess: true,
+                    error: {},
+                    message: "Successfully fetched the Products",
+                    data: response
+        });
+    } catch(error) {
+        console.log("Something went wrong", error);
+    }
+}
+
+
+function getProduct(req, res) { // /api/v1/products/2
+    try {
+        const response = ProductService.getProduct(req.params.id);
+        return res
+                .status(StatusCodes.OK)
+                .json({
+                    sucess: true,
+                    error: {},
+                    message: "Successfully fetched the Product",
+                    data: response
+        });
+    } catch(error) {
+        console.log("Something went wrong", error);
+    }
+}
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProduct
+}
---
+++ src/services/product_service.js
@@ -0,0 +1,29 @@
+const products = [];
+
+class ProductService {
+
+    constructor(respository) {
+        this.respository = respository
+    }
+
+    createProduct(product) {
+        const newProduct = {
+            id: products.length,
+            ...product
+        }
+        products.push(newProduct);
+        return newProduct;
+    }
+    
+    async getProducts() {
+        const response = await this.respository.getProducts();
+        return response.data;
+    }
+    
+    getProduct(id) {
+        return products.filter(product => product.id == id)[0];
+    }
+}
+
+
+module.exports = ProductService
